c080f61473c5a6038af970a8c0b3496785b2ab13
Jack Liu <chechiliu@outlook.com>
2019-08-29 14:40:43 -0700

    Update Aggregate.java

--- Assignment 1/Aggregate.java
+++ Assignment 1/Aggregate.java
@@ -1,70 +1,18 @@
-//Che-Chi Jack Liu
+//Che-Chi (Jack) Liu
 //V00850558
-
 
 import java.io.*;
 import java.util.*;
 import java.util.ArrayList;
 
-public class Aggregate{
+public class Aggregate {
 	public static String[] data;
-
+	
 	public static void showUsage(){
 		System.err.printf("Usage: java Aggregate <function> <aggregation column> <csv file> <group column 1> <group column 2> ...\n");
 		System.err.printf("Where <function> is one of \"count\", \"sum\", \"avg\"\n");	
 	}
-
-
-	/*public static void input(BufferedReader br, String agg_column, String[] group_columns, String[] header_line, int[] group_count, String function) {
-		Scanner input = new Scanner(br);
-		String line = "";
-
-		//to find which column is agg_cloumn
-		int count=0;		
-		for (int i=0; i<header_line.length; i++) {
-			if(agg_column.equals(header_line[i])) {
-				count= i;
-				break;
-			}
-		}
-
-		//print the name of agg_column and groups columns
-		for(int i = 0; i < group_columns.length; i++) {
-			System.out.print(group_columns[i]+",");
-		}
-		System.out.print(function+"("+agg_column+")"+ "\n");
-		
-		
-		while(input.hasNext()) {
-			
-			line = input.nextLine();
-			data = line.split(",");
-
-			//for(int i= 0; i<data.length; i++) {
-			//	System.out.println(data[i]+"ddd");
-			//}
-			//System.out.println(line+"555");
-
-			if(line.trim().isEmpty()) {
-          		continue;
-		   	}
-
-			for(int i= 0; i<group_columns.length; i++) {
-				System.out.print(data[group_count[i]]+",");
-			}
-
-			if(!data[count].matches("^-?\\d*\\.?\\d+$")){   //Assume the agg_column's numerical data are only postitve and negative numbers
-				System.err.printf("The aggregation column contains non-numerical data in this row"+"\n"); 
-			}
-			else {System.out.print(data[count]+"\n");}
-
-		}
-
-		/*for(int i= 0; i<group_count.length; i++) {
-			System.out.println(group_count[i]+",,,,");
-		}*/
-	//}
-
+	
 	public static void input(BufferedReader br, String agg_column, String[] group_columns, String[] header_line, int[] group_count, String function) {
 		Scanner input = new Scanner(br);
 		String line = "";
@@ -72,62 +20,61 @@ public class Aggregate{
 		ArrayList<String> list = new ArrayList<String>();
 
 		//to find which column is agg_cloumn
-		int count=0;		
-		for (int i=0; i<header_line.length; i++) {
+		int count = 0;		
+		for(int i = 0; i < header_line.length; i++) {
 			if(agg_column.equals(header_line[i])) {
 				count= i;
 				break;
 			}
 		}
-
+		
 		//print the name of agg_column and groups columns
 		for(int i = 0; i < group_columns.length; i++) {
 			System.out.print(group_columns[i]+",");
 		}
 		System.out.print(function+"("+agg_column+")"+ "\n");
 
-		
 		int x=0;
 		while(input.hasNext()) {
 			line = input.nextLine();
 			if(line.trim().isEmpty()) {
-          		continue;
+          			continue;
 		   	}
-
+			
 			list.add(line);
-
+			
 			x++; //count the columns
 		}
-
+		
 		int z=0;
 		String[] numbers = new String[x];
 		String[] groupkey= new String[x];
 		String group_key="";
-
+		
 		for(String w: list) {
 			data = w.split(",");
-
-			if(!data[count].matches("^-?\\d*\\.?\\d+$")){   //Assume the agg_column's numerical data are only postitve and negative numbers
+			
+			if(!data[count].matches("^-?\\d*\\.?\\d+$")) {   //Assume the agg_column's numerical data are only postitve and negative numbers
 				System.err.printf("The aggregation column contains non-numerical data in this row"+"\n"); 
 			}else {
 				numbers [z] = data[count];	
 			}
-
-			for(int i= 0; i<group_columns.length; i++) {		
+			
+			for(int i = 0; i < group_columns.length; i++) {		
 				group_key = group_key.concat((data[group_count[i]]));
 			}
-
-			groupkey [z] = group_key;
+			
+			groupkey[z] = group_key;
 			group_key= "";
 
 			String[] data;
 			z++;
 		}
-
+		
 		List<String> stringList = new ArrayList<String>(Arrays.asList(groupkey));
 		HashMap<String, Integer> occurrences = new HashMap<String, Integer>();
 
-		for(String s: stringList){
+		for(String s: stringList) {
 			occurrences.put(s, Collections.frequency(stringList,s));
 		}
 
@@ -136,22 +83,21 @@ public class Aggregate{
 		map.put((groupkey[0]),(numbers[0]));
 		double y;
 		int yy;
-
-		for(int i=1; i<groupkey.length; i++) {
+		
+		for(int i = 1; i < groupkey.length; i++) {
 			String same= groupkey[i];
 			if(function.equals("sum") || function.equals("avg")) {
 				if(map.containsKey(same)) {	
 					if(numbers[i].contains(".")) {
-            			y = (Double.parseDouble(map.get(same))) + (Double.parseDouble(numbers[i]));
-            			map.put(groupkey[i], (Double.toString(y)));
-            			y=0;
-        			}
-      				else{
-      					yy = (Integer.parseInt((map.get(same)))) + (Integer.parseInt(numbers[i]));
-            			map.put(groupkey[i], (Integer.toString(yy)));
-            			yy=0;
-      				}	
-				}else{
+            					y = (Double.parseDouble(map.get(same))) + (Double.parseDouble(numbers[i]));
+            					map.put(groupkey[i], (Double.toString(y)));
+            					y=0;
+        				}else {
+      						yy = (Integer.parseInt((map.get(same)))) + (Integer.parseInt(numbers[i]));
+            					map.put(groupkey[i], (Integer.toString(yy)));
+            					yy=0;
+      					}	
+				}else {
 					map.put((groupkey[i]),(numbers[i]));
 				}
 			}
@@ -160,31 +106,21 @@ public class Aggregate{
 			}
 			same="";
 		}
-
-		/*System.out.println(occurrences.keySet()+" This is the result of after grouping with group_columns");
-		System.out.println(map.values()+" This is the result of function 'sum', sum up all elements in the aggregation column within each group");
-		System.out.println(occurrences.values()+" This is the result of function 'count', the number of rows in each group");*/
-
+		
 		if(function.equals("count")) {
+    			System.out.println(occurrences.keySet());
 
-    		System.out.println(occurrences.keySet());
-
-			for (Integer value : occurrences.values()) {
-    			System.out.print(value+",");
+			for(Integer value : occurrences.values()) {
+    				System.out.print(value+",");
 			}
 		}
 
 		if(function.equals("sum")) {
-
-    		System.out.println(occurrences.keySet());
-
-		
-    		System.out.print(map.values()+",");
-			
+    			System.out.println(occurrences.keySet());
+    			System.out.print(map.values()+",");
 		}
 	}
-
-
+	
 	public static void main(String[] args){
 		//At least four arguments are needed
 		if (args.length < 4){
@@ -195,72 +131,68 @@ public class Aggregate{
 		String agg_function = args[0];
 		String agg_column = args[1];
 		String csv_filename = args[2];
-
+		
 		int[] group_count = new int[args.length - 3];
 		String[] group_columns = new String[args.length - 3];
 		for(int i = 3; i < args.length; i++) {
 			group_columns[i-3] = args[i];
-			if (agg_column.equals(args[i])) {
+			if(agg_column.equals(args[i])) {
 				showUsage();
 				System.err.printf("A column can not be used as both a grouping column and an aggregation column.");
 				return;
 			}
 		}
 
-		if (!agg_function.equals("count") && !agg_function.equals("sum") && !agg_function.equals("avg")){
+		if(!agg_function.equals("count") && !agg_function.equals("sum") && !agg_function.equals("avg")) {
 			showUsage();
 			return;
 		}
 		
 		BufferedReader br = null;
 		
-		try{
+		try {
 			br = new BufferedReader(new FileReader(csv_filename));
-		}catch( IOException e ){
+		}catch( IOException e ) {
 			System.err.printf("Error: Unable to open file %s\n", csv_filename);
 			return;
 		}
 		
 		String header_line;
 
-		try{
+		try {
 			header_line = br.readLine(); //The readLine method returns either the next line of the file or null (if the end of the file has been reached)
-		} catch (IOException e){
+		}catch (IOException e) {
 			System.err.printf("Error reading file\n", csv_filename);
 			return;
 		}
-		if (header_line == null){
+		
+		if(header_line == null) {
 			System.err.printf("Error: CSV file %s has no header row\n", csv_filename);
 			return;
 		}
 		
-		/*int commas = 0;
-		for(int i = 0; i < header_line.length(); i++) {
-    		if(header_line.charAt(i) == ','){
-    			commas++;
-    		}
-		}*/
-
 		int count=0;
 		//Split the header_line string into an array of string values using a comma
 		//as the separator.
 		String[] column_names = header_line.split(",");
 		//found out which group columns to print 
-		for(int i = 0; i<column_names.length; i++){
-			for(int j = 0; j<group_columns.length; j++){
+		for(int i = 0; i < column_names.length; i++) {
+			for(int j = 0; j < group_columns.length; j++) {
 				if(column_names[i].equals(group_columns[j])) {
 					group_count[count] = i;
 					count++;
 				}
 			}
 		}
-
+		
 		boolean agg_found = false;
-		for (String s: column_names) {
-			if (agg_column.equals(s))  //assuming there is only one agg_column 
+		for(String s: column_names) {
+			if(agg_column.equals(s)) {  //assuming there is only one agg_column 
 				agg_found = true;
+			}	
 		}
-		if (!agg_found) {
+		
+		if(!agg_found) {
 			System.err.printf("Error: The aggregation "+ agg_column+ " column could not be found in the file");
 			return;
 		}
@@ -271,10 +203,7 @@ public class Aggregate{
 				return;
 			}
 		}
-
+		
 		input(br, agg_column, group_columns, column_names, group_count, agg_function);
-		//input(br);
 	}
-	
-
 }
